c99ba65d497a5ff478b133773e0c0163fc47cdf3
Grigorii Chudnov <g.chudnov@gmail.com>
2020-06-11 10:32:45 +0200

    update build

--- native-image-build.sh
+++ native-image-build.sh
@@ -3,8 +3,6 @@
 sbt "test; cli/assembly"
 sbt "cli/assembly"
 
-cp ./target/kprojekt-cli.jar /home/gchudnov/Downloads/bld/artifact/kprojekt-cli.jar
-
 native-image \
   --verbose \
   --initialize-at-build-time \
@@ -12,5 +10,5 @@ native-image \
   --allow-incomplete-classpath \
   -H:+ReportUnsupportedElementsAtRuntime \
   -H:+ReportExceptionStackTraces \
-  -H:ResourceConfigurationFiles=/home/gchudnov/Projects/kprojekt/res/gvm-resources.json \
-  -jar ./artifact/kprojekt-cli.jar kprojekt-cli
+  -H:ResourceConfigurationFiles=./res/graalvm/resources.json \
+  -jar ./target/kprojekt-cli.jar kprojekt-cli
