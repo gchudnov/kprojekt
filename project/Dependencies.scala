0b69577b7f219a336b7e99e3d7c4e697aa96707e
Grigorii Chudnov <g.chudnov@gmail.com>
2021-10-06 23:57:05 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object Dependencies {
 
   object versions {
-    val fastparse     = "2.3.2"
+    val fastparse     = "2.3.3"
     val kafka         = "2.8.0"
     val kindProjector = "0.10.3"
-    val logback       = "1.2.5"
-    val pureConfig    = "0.16.0"
+    val logback       = "1.2.6"
+    val pureConfig    = "0.17.0"
     val scopt         = "4.0.1"
-    val zio           = "1.0.11"
-    val zioLogging    = "0.5.11"
+    val zio           = "1.0.12"
+    val zioLogging    = "0.5.12"
   }
 
   private val compiler = Seq(
