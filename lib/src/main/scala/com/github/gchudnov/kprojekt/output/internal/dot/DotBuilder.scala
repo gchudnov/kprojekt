46fc1d31fc927639520de925dada5e1ebd0ad910
Grigorii Chudnov <g.chudnov@gmail.com>
2023-02-21 22:14:51 +0100

    start dot builder

--- lib/src/main/scala/com/github/gchudnov/kprojekt/output/internal/dot/DotBuilder.scala
+++ lib/src/main/scala/com/github/gchudnov/kprojekt/output/internal/dot/DotBuilder.scala
@@ -1,5 +1,7 @@
 package com.github.gchudnov.kprojekt.output.internal.dot
 
+import com.github.gchudnov.kprojekt.output.Id
+import com.github.gchudnov.kprojekt.output.Edge
 import com.github.gchudnov.kprojekt.output.Builder
 
 /**
@@ -7,5 +9,47 @@ import com.github.gchudnov.kprojekt.output.Builder
   *
   */
 final class DotBuilder() extends Builder {
+
+  override def build: String = 
+    ???
+
+  override def topologyStart(name: String): Builder =
+    ???
+
+  override def topologyEnd(): Builder =
+    ???
+
+  override def topic(id: Id): Builder = 
+    ???
+
+  override def subtopologyStart(name: String): Builder = 
+    ???
+
+  override def subtopologyEnd(): Builder = 
+    ???
+
+  override def edge(e: Edge): Builder =
+    ???
+
+  override def source(id: Id, topics: Iterable[Id]): Builder =
+    ???
+
+  override def processor(id: Id, stores: Iterable[Id]): Builder =
+    ???
+
+  override def sink(id: Id, topic: Id): Builder =
+    ???
+
+  override def storeEdges(edges: Iterable[Edge]): Builder =
+    ???
+
+  override def store(id: Id): Builder =
+    ???
+
+  override def repository(ns: Iterable[Id]): Builder =
+    ???
+
+  override def rank(a: Id, b: Id): Builder =
+    ???
 
 }
